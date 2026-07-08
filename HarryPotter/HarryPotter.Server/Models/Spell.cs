662a3361713f1104038c4a2d9b1c455088921628
Alfie-Spooner <djalfsta@outlook.com>
2026-07-08 11:26:56 +0100

    Added spell class

--- HarryPotter/HarryPotter.Server/Models/Spell.cs
+++ HarryPotter/HarryPotter.Server/Models/Spell.cs
@@ -1,6 +1,17 @@
-﻿namespace HarryPotter.Server.Models
+﻿using System.Text.Json.Serialization;
+
+namespace HarryPotter.Server.Models
 {
     public class Spell
     {
+        [JsonPropertyName("id")]
+        public string? ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
     }
 }
+
