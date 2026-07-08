85ad0541f89b38a45bc05a0a40d761b5f910714c
Alfie <djalfsta@outlook.com>
2026-07-08 12:02:34 +0100

    Create Spell.cs
    I have made the client side for the spell class

---
+++ HarryPotter/HarryPotter.Client/Core/Models/Spell.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HarryPotter.Client.Core.Models
+{
+    public class Spell
+    {
+        [JsonPropertyName("id")]
+        public string? ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+    }
+}
+
